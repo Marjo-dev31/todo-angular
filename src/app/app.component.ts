19fdca5ee0abd3c3b79093baec4661ee47c4eb3f
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-12-18 17:49:40 +0100

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HearderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HearderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
