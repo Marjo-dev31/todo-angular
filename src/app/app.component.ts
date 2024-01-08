f4621a7e19fa0da3f37d8888e9ce1e2168877627
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-01-08 15:54:20 +0100

    input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { HearderComponent } from './header/header.component';
 import { TodolistComponent } from './main/todolist/todolist.component'
 import { DidlistComponent } from './main/didlist/didlist.component'
 import { TopHeaderComponent} from './top-header/top-header.component'
+import { AddTaskComponent } from './main/add-task/add-task.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HearderComponent, TodolistComponent, DidlistComponent, TopHeaderComponent],
+  imports: [CommonModule, RouterOutlet, HearderComponent, TodolistComponent, DidlistComponent, TopHeaderComponent, AddTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
